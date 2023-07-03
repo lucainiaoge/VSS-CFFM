f6f1e93480f15b30513bde806f1a50bd150b35d1
tonglu <tonglu@ethz.student.ch>
2023-07-03 20:51:54 +0200

    debug data resolution

--- local_configs/_base_/datasets/cityscapes_512x512_repeat_clips.py
+++ local_configs/_base_/datasets/cityscapes_512x512_repeat_clips.py
@@ -20,7 +20,8 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(2048, 1024),
+        # img_scale=(2048, 1024),
+        img_scale=(1024, 512),
         # img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5, 1.75],
         flip=False,
         transforms=[
