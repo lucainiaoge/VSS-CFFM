398918ad0e3f75a79a004f37e646229f75d031fb
tonglu <tonglu@ethz.student.ch>
2023-09-01 10:55:48 +0200

    hr cffm

--- local_configs/_base_/datasets/cityscapes_1024x1024_repeat_clips.py
+++ local_configs/_base_/datasets/cityscapes_1024x1024_repeat_clips.py
@@ -3,11 +3,11 @@ dataset_type = 'CityscapesDataset_clips'
 data_root = 'data/cityscapes/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
-crop_size = (512, 1024)
+crop_size = (1024, 1024)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations'),
-    dict(type='Resize', img_scale=(1024, 512), ratio_range=(0.5, 2.0), process_clips=True),
+    dict(type='Resize', img_scale=(2048, 1024), ratio_range=(0.5, 2.0), process_clips=True),
     dict(type='RandomCrop_clips', crop_size=crop_size, cat_max_ratio=0.75),
     dict(type='RandomFlip_clips', prob=0.5),
     dict(type='PhotoMetricDistortion_clips'),
@@ -20,7 +20,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(1024, 512),
+        img_scale=(2048, 1024),
         # img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5, 1.75],
         flip=False,
         transforms=[
@@ -32,7 +32,7 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    samples_per_gpu=2,
+    samples_per_gpu=1,
     workers_per_gpu=5,
     train=dict(
         type='RepeatDataset',
